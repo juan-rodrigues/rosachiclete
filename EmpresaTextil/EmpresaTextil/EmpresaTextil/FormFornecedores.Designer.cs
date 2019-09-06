ccd0d0005d90c0f902711f309415daab8de7c99d
Aluno <Aluno@LF105M21-72099>
2019-09-06 09:47:09 -0300

    mudança teste

--- EmpresaTextil/EmpresaTextil/EmpresaTextil/FormFornecedores.Designer.cs
+++ EmpresaTextil/EmpresaTextil/EmpresaTextil/FormFornecedores.Designer.cs
@@ -9,7 +9,7 @@
 
         /// <summary>
         /// Clean up any resources being used.
-        /// </summary>
+        /// </summary>dadadadadadadada ué
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
